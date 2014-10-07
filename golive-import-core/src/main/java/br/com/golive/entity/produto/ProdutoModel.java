9645acd5e8a28de961ec5c713b617c2606d1b9ce
Maruen Mehana <maruen@gmail.com>
2014-10-07 20:07:29 -0300

    Inclusão da Tela Cadastro de Cores

--- golive-import-core/src/main/java/br/com/golive/entity/produto/ProdutoModel.java
+++ golive-import-core/src/main/java/br/com/golive/entity/produto/ProdutoModel.java
@@ -4,12 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import br.com.golive.annotation.Jasper;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 
-@Data
-@EqualsAndHashCode(callSuper=false)
 @Jasper(titulo="Produto",
 		nomeDoArquivoGerado="Produto",
 		nomeArquivoJasper ="Produto"
@@ -41,6 +37,110 @@ public class ProdutoModel {
 		this.id = id;
 		this.dataInclusao = dataInclusao;
 		this.dataAlteracao = dataAlteracao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDataInclusao() {
+		return dataInclusao;
+	}
+
+	public void setDataInclusao(Date dataInclusao) {
+		this.dataInclusao = dataInclusao;
+	}
+
+	public Date getDataAlteracao() {
+		return dataAlteracao;
+	}
+
+	public void setDataAlteracao(Date dataAlteracao) {
+		this.dataAlteracao = dataAlteracao;
+	}
+
+	public Long getCodigoProduto() {
+		return codigoProduto;
+	}
+
+	public void setCodigoProduto(Long codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public List<String> getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(List<String> unidades) {
+		this.unidades = unidades;
+	}
+
+	public List<String> getCodigos() {
+		return codigos;
+	}
+
+	public void setCodigos(List<String> codigos) {
+		this.codigos = codigos;
+	}
+
+	public List<String> getDescricoes() {
+		return descricoes;
+	}
+
+	public void setDescricoes(List<String> descricoes) {
+		this.descricoes = descricoes;
+	}
+
+	public List<String> getColecoes() {
+		return colecoes;
+	}
+
+	public void setColecoes(List<String> colecoes) {
+		this.colecoes = colecoes;
+	}
+
+	public List<String> getCodigoBarras() {
+		return codigoBarras;
+	}
+
+	public void setCodigoBarras(List<String> codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+
+	public List<String> getCores() {
+		return cores;
+	}
+
+	public void setCores(List<String> cores) {
+		this.cores = cores;
+	}
+
+	public List<String> getPadraoLargura() {
+		return padraoLargura;
+	}
+
+	public void setPadraoLargura(List<String> padraoLargura) {
+		this.padraoLargura = padraoLargura;
+	}
+
+	public List<String> getPadraoEspessura() {
+		return padraoEspessura;
+	}
+
+	public void setPadraoEspessura(List<String> padraoEspessura) {
+		this.padraoEspessura = padraoEspessura;
 	}
 
 	
