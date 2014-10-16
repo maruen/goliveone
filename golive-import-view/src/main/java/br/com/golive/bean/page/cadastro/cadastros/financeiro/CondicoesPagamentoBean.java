461d4666c42b2c03e4c25d9178392a016e7677f5
Suellen Gularte Cruz <suellen.cruz@golivebusiness.com.br>
2014-10-16 17:47:37 -0300

    Cadastro de Condições de Pagamento Cadastro de Departamentos Cadastro de Estados Cadastro de Formas de Pagamento

---
+++ golive-import-view/src/main/java/br/com/golive/bean/page/cadastro/cadastros/financeiro/CondicoesPagamentoBean.java
@@ -0,0 +1,90 @@
+package br.com.golive.bean.page.cadastro.cadastros.financeiro;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import br.com.golive.annotation.Label;
+import br.com.golive.bean.page.cadastro.rules.CadastroBeanRules;
+import br.com.golive.entity.financeiro.condicoespagamento.CondicoesPagamentoModel;
+import br.com.golive.qualifier.LabelSystemInjected;
+import br.com.golive.utils.GoliveOneProperties;
+
+@Label(name = "label.cadastroCondicoesPagamento")
+@ManagedBean
+@ViewScoped
+public class CondicoesPagamentoBean extends CadastroBeanRules<CondicoesPagamentoModel> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Logger logger;
+	
+	@Inject
+	@LabelSystemInjected
+	private GoliveOneProperties labels;
+
+	@Override
+	@PostConstruct
+	public void init() {
+		super.init(criarLista());
+		
+		logger.info("Inicializando = {}", this.getClass().getName());
+	}
+
+	private List<CondicoesPagamentoModel> criarLista() {
+		final List<CondicoesPagamentoModel> lista = new ArrayList<CondicoesPagamentoModel>();
+			
+		for (Integer i = 0; i <= 10; i++){
+			lista.add(new CondicoesPagamentoModel(new Long(i), new Date(), new Date(), "28 / 56 dd"));
+		}
+		
+		return lista;
+	}
+
+	@Override
+	protected Logger getLogger() {
+		return logger;
+	}
+	
+	@Override
+	public void salvar(){
+		super.salvar();
+		logger.info("Salvando = {}");
+	}
+	
+	public List<Object> getAuditoriaLogs() {
+		ArrayList<Object> list = new ArrayList<Object>();
+		
+		list.add(new Object());
+		list.add(new Object());
+		list.add(new Object());
+
+		return list;
+	}
+	
+	public List<Object> getControleVencimentoDataTable(){
+		ArrayList<Object> lista = new ArrayList<Object>();
+		
+		lista.add(new Object());
+		lista.add(new Object());
+		lista.add(new Object());
+		
+		return lista;
+	}
+	
+	public void salvarControleVencimento(){
+		logger.info("Salvar controle vencimento");
+	}
+	
+	public void excluirControleVencimento(){
+		logger.info("Excluir controle vencimento");
+	}
+}
