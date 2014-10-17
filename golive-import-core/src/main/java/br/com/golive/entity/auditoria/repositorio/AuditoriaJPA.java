ada95ecf45cb85166c5f7139120221113c0599a1
Maruen Mehana <maruen@gmail.com>
2014-10-17 14:37:03 -0300

    Auditoria e Refactorings

--- golive-import-core/src/main/java/br/com/golive/entity/auditoria/repositorio/AuditoriaJPA.java
+++
@@ -1,8 +0,0 @@
-package br.com.golive.entity.auditoria.repositorio;
-
-import br.com.golive.entity.auditoria.model.AuditoriaModel;
-import br.com.golive.jpa.JpaGoLive;
-
-public class AuditoriaJPA extends JpaGoLive<AuditoriaModel, Long>{
-	
-}
