0d1c9d0feb05db1db6d37b76dfb6e6948f263aa9
Maruen Mehana <maruen@gmail.com>
2014-10-28 13:12:46 -0200

    Fixings JpaGoLive

--- golive-import-core/src/main/java/br/com/golive/jpa/JpaGoLive.java
+++ golive-import-core/src/main/java/br/com/golive/jpa/JpaGoLive.java
@@ -313,7 +313,6 @@ public abstract class JpaGoLive<T extends Serializable, I extends Object> {
 	public void update(final T classe) {
 		if (verifyAnnotation(classe)) {
 			merge(classe);
-			save(classe);
 		}
 	}
 
